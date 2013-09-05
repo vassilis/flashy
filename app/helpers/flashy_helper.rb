c8116a8718d7780b1baff5c8f8d0d420c56da91e
vassilis <vas.terzo@gmail.com>
2013-09-06 02:40:46 +0300

    fixed bootstrap 3 compatibility issues

--- app/helpers/flashy_helper.rb
+++ app/helpers/flashy_helper.rb
@@ -1,5 +1,15 @@
 module FlashyHelper
-  def flashy
-    render partial: 'flashy/flash'
-  end
+	def flashy
+		render partial: 'flashy/flash'
+	end
+
+	def flash_name(name)
+		keys = {
+			success: 'success',
+			notice: 'info',
+			alert: 'warning',
+			error: 'danger'
+		}
+		keys[name]
+	end
 end
